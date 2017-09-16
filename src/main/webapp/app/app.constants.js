6fc5d7d3b989e0d21ed6228ccc7ecec191d122fc
Adnan Khan <Adnan2103@hotmail.com>
2017-09-16 22:09:45 +0530

    Stage 1

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -4,7 +4,7 @@
     angular
         .module('dentoCareApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
-        .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1505537720030)
+        .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
 ;
 })();
